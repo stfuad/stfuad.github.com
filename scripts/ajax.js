aa7367f1999fd889a02f2f00df6b8289a3c8fd73
stfuad <36175258+stfuad@users.noreply.github.com>
2019-06-22 22:58:26 -0400

    Update ajax.js

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -1,15 +1,17 @@
 export default function GetJSON(...paths) {
-    let request = new XMLHttpRequest();
-    request.onload = function() {
-        paths.forEach(file => {
-            let parsed = JSON.parse(request.responseText);
+    paths.forEach(file => {
+        let request = new XMLHttpRequest();
+        request.onload = function() {
+            
+                let parsed = JSON.parse(request.responseText);
 
-            for(let key in parsed) {
-                localStorage.setItem(key, JSON.stringify(parsed[key]));
-            }
-        });
-        
-    };
-    request.open('GET', path, true);
-    request.send();
+                for(let key in parsed) {
+                    localStorage.setItem(key, JSON.stringify(parsed[key]));
+                }
+            
+            
+        };
+        request.open('GET', paths, true);
+        request.send();
+    });
 }
