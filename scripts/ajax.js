021ffe7e5d4421764f443752cf367c0072f4d9a2
stfuad <36175258+stfuad@users.noreply.github.com>
2019-06-22 23:38:55 -0400

    something

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -1,12 +1,8 @@
 export function GetJSON(path) {
-    console.log(path);
-
     let request = new XMLHttpRequest();
     request.onload = function() {
-        console.log("Loaded");
 
         let parsed = JSON.parse(request.responseText);
-        console.log(parsed);
 
         for(let key in parsed) {
             localStorage.setItem(key, JSON.stringify(parsed[key]));
