ca0403a02e28ed9383c8a69da917eb6e67d82549
stfuad <36175258+stfuad@users.noreply.github.com>
2019-08-09 17:02:37 -0400

    up

--- classes/pages.classes.js
+++ classes/pages.classes.js
@@ -651,8 +651,15 @@ export class Bestiary extends HTMLElement {
 
         let shadow = this.attachShadow({mode: 'open'});
 
-        let style = Element('style', shadow);
+        let style = document.createElement('style');
         style.textContent = `
+            :host {
+                display: grid;
+                grid-template-columns: 300px 1fr;
+                grid-template-rows: 22px calc(100vh - 32px);
+                grid-gap: 10px;
+            }
+
             .header {
                 display: block;
                 font-weight: bold;
@@ -660,13 +667,6 @@ export class Bestiary extends HTMLElement {
                 padding-left: 10px;
                 border-top: 1px solid black;
                 border-bottom: 1px solid black;
-            }
-
-            .grid {
-                display: grid;
-                grid-template-columns: 300px 1fr;
-                grid-template-rows: 22px calc(100vh - 32px);
-                grid-gap: 10px;
             }
 
             #tabs > button {
@@ -697,17 +697,22 @@ export class Bestiary extends HTMLElement {
             }
         `;
 
-        let container = Element('div', shadow);
-        container.className = "grid";
+            shadow.appendChild(style);
 
-        let tabs = Element('div', container);
+        let tabs = document.createElement('div');
         tabs.id = "tabs";
 
-        let list = Element('div', container);
+        shadow.appendChild(tabs);
+
+        let list = document.createElement('div');
         list.id = "list";
     
-        let subContent = Element('div', container);
+        shadow.appendChild(list);
+
+        let subContent = document.createElement('div');
         subContent.id = "subContent";
+
+        shadow.appendChild(subContent);
 
         let bestiary = JSON.parse(localStorage.getItem("Bestiary"));
 
@@ -718,46 +723,62 @@ export class Bestiary extends HTMLElement {
 
         // buttons
 
-        let byName = Button("by Name", tabs);
-        byName.addEventListener('click', () => {
-            list.innerHTML = "";
-            CreateList(sortedByName);
-        }, false);
+        let byName = document.createElement('button');
+        byName.appendChild(document.createTextNode("by Name"));
+        byName.onclick = () => Wrapper(sortedByName);
 
-        let byType = Button("by Type", tabs);
-        byType.addEventListener('click', () => {
-            list.innerHTML = "";
-            CreateList(sortedByType);
-        }, false);
+        tabs.appendChild(byName);
 
-        let byCR = Button("by CR", tabs);
-        byCR.addEventListener('click', () => {
-            list.innerHTML = "";
-            CreateList(sortedByCR);
-        }, false);
+        let byType = document.createElement('button');
+        byType.appendChild(document.createTextNode("by Type"));
+        byType.onclick = () => Wrapper(sortedByType);
 
-        let byBook = Button("by Book", tabs);
-        byBook.addEventListener('click', () => {
-            list.innerHTML = "";
-            CreateList(sortedByBook);
-        }, false);
+        tabs.appendChild(byType);
+
+        let byCR = document.createElement('button');
+        byCR.appendChild(document.createTextNode("by CR"));
+        byCR.onclick = () => Wrapper(sortedByCR);
+
+        tabs.appendChild(byCR);
+
+        let byBook = document.createElement('button');
+        byBook.appendChild(document.createTextNode("by Book"));
+        byBook.onclick = () => Wrapper(sortedByBook);
+
+        tabs.appendChild(byBook);
 
         CreateList(sortedByName);
 
+        function Wrapper(obj) {
+            let list = shadow.querySelector("#list");
+
+            if (list.childElementCount > 0) {
+                while (list.firstChild) {
+                    list.firstChild.remove();
+                }
+            }
+
+            CreateList(obj);
+        }
+
         function CreateList(obj) {
             for (let array in obj) {
-                let div = Element('div', list);
+                let div = document.createElement('div');
         
                 if (obj[array].length > 0) {
-                    let span = Element('span', div);
+                    let span = document.createElement('span');
                     span.appendChild(document.createTextNode(array));
                     span.className = "header";
+
+                    div.appendChild(span);
                 }
                 
-                obj[array].sort().forEach(item => {
-        
-                    let a = Link(item, undefined, div)
-                    a.addEventListener('click', () => {
+                let sorted = obj[array].sort();
+
+                sorted.forEach(item => {
+                    let a = document.createElement('a');
+                    a.appendChild(document.createTextNode(item));
+                    a.onclick = () => {
                         let target = shadow.querySelector("creature-sheet");
         
                         if (target !== null) {
@@ -765,7 +786,9 @@ export class Bestiary extends HTMLElement {
                         }
                         
                         subContent.appendChild(new sheet.CreatureSheet(item, bestiary[item]));
-                    });
+                    };
+
+                    div.appendChild(a);
                 });
         
                 list.appendChild(div);
