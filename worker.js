afbff3f6fb6b7748906ca39c389b253ce068868e
stfuad <36175258+stfuad@users.noreply.github.com>
2019-10-06 09:48:36 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,33 +1,35 @@
 self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open("stfuad.cc")
-        .then((cache) => {
-            return cache.addAll([
-                "./",
-                "./bestiary.html",
-                "./classes.html",
-                "./feats.html",
-                "./index.html",
-                "./initiative.html",
-                "./magicItems.html",
-                "./navigation.html",
-                "./races.html",
-                "./spells.html",
-                "./classes/sheets.classes.js",
-                "./classes/spellModal.class.js",
-                "./json/5e Data.json",
-                "./modules/bestiary.module.js",
-                "./modules/classes.module.js",
-                "./modules/creature.module.js",
-                "./modules/feats.module.js",
-                "./modules/htmlElements.module.js",
-                "./modules/item.module.js",
-                "./modules/magicItems.module.js",
-                "./modules/races.module.js",
-                "./modules/sorting.module.js",
-                "./modules/spell.module.js",
-                "./modules/spells.module.js"
-            ]);
-    }));
+    e.waitUntil(
+        caches.open("stfuad.cc")
+            .then((cache) => {
+                return cache.addAll([
+                    "./",
+                    "./bestiary.html",
+                    "./classes.html",
+                    "./feats.html",
+                    "./index.html",
+                    "./initiative.html",
+                    "./magicItems.html",
+                    "./navigation.html",
+                    "./races.html",
+                    "./spells.html",
+                    "./classes/sheets.classes.js",
+                    "./classes/spellModal.class.js",
+                    "./json/5e Data.json",
+                    "./modules/bestiary.module.js",
+                    "./modules/classes.module.js",
+                    "./modules/creature.module.js",
+                    "./modules/feats.module.js",
+                    "./modules/htmlElements.module.js",
+                    "./modules/item.module.js",
+                    "./modules/magicItems.module.js",
+                    "./modules/races.module.js",
+                    "./modules/sorting.module.js",
+                    "./modules/spell.module.js",
+                    "./modules/spells.module.js"
+                ]);
+            })
+    );
 
     console.log("install success")
 });
